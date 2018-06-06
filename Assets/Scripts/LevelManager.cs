9d9089bc7b368a111f2378aa475d08f780caa7e8
thegreatgabbi <onethousand.words.88@gmail.com>
2018-06-06 22:21:59 +0800

    initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name) {
+        Debug.Log("Load request for " + name);
+        SceneManager.LoadScene(name);
+    }
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested.");
+        Application.Quit();
+    }
+}
---
+++ Assets/Sounds/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    static MusicPlayer instance = null;
+
+    private void Awake()
+    {
+        Debug.Log("Music player Awake" + GetInstanceID());
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("Duplicate music player self-destructing!");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
